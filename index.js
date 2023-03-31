8cd7b359c541afb41668ce34835b2d175f1194c9
Felix Nehrke <felix@nehrke.info>
2023-03-31 17:43:07 +0200

    Add a test-suite to determine the correctness of the implementation

---
+++ index.js
@@ -0,0 +1,5 @@
+const lengthOf = (arr) => {
+  return arr.length;
+}
+
+module.exports = { lengthOf };
---
+++ index.spec.js
@@ -0,0 +1,119 @@
+const { lengthOf } = require('./index');
+
+it(
+  'should calculate 0 for empty arrays',
+  () => expect(lengthOf(arrayWithDefinedLength([]))).toBe(0)
+);
+it(
+  'should calculate the correct length for array of numbers',
+  () => expect(lengthOf(arrayWithDefinedLength([1,2,3]))).toBe(3)
+);
+it(
+  'should calculate the correct length for array of zeros',
+  () => expect(lengthOf(arrayWithDefinedLength([0, 0, 0, 0]))).toBe(4)
+);
+it(
+  'should calculate the correct length for array of strings',
+  () => expect(lengthOf(arrayWithDefinedLength(['hello', 'world']))).toBe(2)
+);
+it(
+  'should calculate the correct length for array of empty strings',
+  () => expect(lengthOf(arrayWithDefinedLength(['', '']))).toBe(2)
+);
+it(
+  'should calculate the correct length of a constructed Array',
+  () => expect(lengthOf(arrayWithDefinedLength(new Array(10)))).toBe(10)
+);
+it(
+  'should calculate the correct length for array of undefined',
+  () => expect(lengthOf(arrayWithDefinedLength([undefined, undefined, undefined]))).toBe(3)
+);
+it(
+  'should calculate the correct length for array of null',
+  () => expect(lengthOf(arrayWithDefinedLength([null, null, null, null]))).toBe(4)
+);
+it(
+  'should calculate the correct length for array of empty spots',
+  () => expect(lengthOf(arrayWithDefinedLength([,,,]))).toBe(3)
+);
+it(
+  'should calculate the correct length for array ending with empty spots',
+  () => expect(lengthOf(arrayWithDefinedLength([1,2,3,,,]))).toBe(5)
+);
+
+function arrayWithDefinedLength(arr) {
+  const mock = [...arr];
+  const readLength = arr.length;
+  Object.defineProperty(arr, 'length', {
+    value: 2,
+    writable: false
+  });
+  arr.at = (...args) => mock.at.apply(mock, args)
+  arr.concat = (...args) => mock.concat.apply(mock, args)
+  arr.copyWithin = (...args) => mock.copyWithin.apply(mock, args)
+  arr.entries = (...args) => mock.entries.apply(mock, args)
+  arr.every = (...args) => mock.every.apply(mock, args)
+  arr.fill = (...args) => mock.fill.apply(mock, args)
+  arr.filter = (...args) => mock.filter.apply(mock, args)
+  arr.find = (...args) => mock.find.apply(mock, args)
+  arr.findIndex = (...args) => mock.findIndex.apply(mock, args)
+  arr.findLast = (...args) => mock.findLast.apply(mock, args)
+  arr.findLastIndex = (...args) => mock.findLastIndex.apply(mock, args)
+  arr.flat = (...args) => mock.flat.apply(mock, args)
+  arr.flatMap = (...args) => mock.flatMap.apply(mock, args)
+  arr.forEach = (...args) => mock.forEach.apply(mock, args)
+  arr.group = (...args) => mock.group.apply(mock, args)
+  arr.groupToMap = (...args) => mock.groupToMap.apply(mock, args)
+  arr.includes = (...args) => mock.includes.apply(mock, args)
+  arr.indexOf = (...args) => mock.indexOf.apply(mock, args)
+  arr.join = (...args) => mock.join.apply(mock, args)
+  arr.keys = (...args) => mock.keys.apply(mock, args)
+  arr.lastIndexOf = (...args) => mock.lastIndexOf.apply(mock, args)
+  arr.map = (...args) => mock.map.apply(mock, args)
+  arr.pop = (...args) => mock.pop.apply(mock, args)
+  arr.push = (...args) => mock.push.apply(mock, args)
+  arr.reduce = (...args) => mock.reduce.apply(mock, args)
+  arr.reduceRight = (...args) => mock.reduceRight.apply(mock, args)
+  arr.reverse = (...args) => mock.reverse.apply(mock, args)
+  arr.shift = (...args) => mock.shift.apply(mock, args)
+  arr.slice = (...args) => mock.slice.apply(mock, args)
+  arr.some = (...args) => mock.some.apply(mock, args)
+  arr.sort = (...args) => mock.sort.apply(mock, args)
+  arr.splice = (...args) => mock.splice.apply(mock, args)
+  arr.toLocaleString = (...args) => mock.toLocaleString.apply(mock, args)
+  arr.toString = (...args) => mock.toString.apply(mock, args)
+  arr.unshift = (...args) => mock.unshift.apply(mock, args)
+  arr.values = (...args) => mock.values.apply(mock, args)
+  return arr;
+}
+
+
+function it(description, fn) {
+  try {
+    fn();
+    console.log(`it ${description}`);
+  } catch(e) {
+    console.error(`it ${description}`);
+    console.error(`  ${e.message}`);
+  }
+}
+
+function expect(value) {
+  const assert = (v1, v2) => {
+    if (v1 !== v2)
+      throw new Error(`expected ${v2} but received ${v1}`);
+  }
+  return {
+    toBe: (expectedValue) => assert(value, expectedValue),
+    toThrow: (errorType) => {
+      errorType = errorType ?? Error
+      let result;
+      try {
+        value();
+      } catch(e) {
+        result = e.constructor
+      }
+      assert(errorType, result)
+    }
+  }
+}
