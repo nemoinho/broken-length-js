0c0ce977a9d66a552082600fe9bcdf98b95413a0
Felix Nehrke <felix@nehrke.info>
2023-05-03 15:09:50 +0200

    Add a solution with explanation

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const lengthOf = (arr) => {
-  return arr.length;
+  return arr // example: [3,,"f",,]
+    .map(_ => 1) // make every element a 1, example: [1,,1,,]
+    .toString() // convert the array to a string, example: "1,,1,"
+    // The next step is mandatory to distinuish between single-element and empty arrays
+    .replace(/^(.)/, ',$1') // prepend the string with ","; example: ",1,,1,"
+    .split(',') // make an array again ['','1','','1','']
+    .reduce(y => ++y, -1) // count (starting with -1, because of prepending): 4
 }
 
 module.exports = { lengthOf };
